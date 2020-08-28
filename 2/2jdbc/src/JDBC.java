3ff2b6720acffac78c515e056718089898b41b4a
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-08-28 13:15:04 +0530

    :art:

---
+++ 2/2jdbc/src/JDBC.java
@@ -0,0 +1,18 @@
+import java.sql.*;
+
+public class JDBC {
+	public static void main(String[] args) throws Exception {
+		Class.forName("com.mysql.jbdc.Driver");
+		Connection con = DriverManager.getConnection("jbdc:mysql://localhost:3306/second31155","root","2368");
+		
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery("Select * from Departments where dept_id=1");
+		
+		rs.next();
+		int dept_id=rs.getInt("Dept_id");
+		String dept_name=rs.getString("Dep_Name");
+		System.out.println(dept_id+" "+ dept_name);
+		st.close();
+		con.close();
+	}
+}
