2f0d36ee37f873d09587918af106a223ffba8802
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-09-04 02:53:13 +0530

    master: Added

--- 2/2jdbc/src/JDBC.java
+++ 2/2jdbc/src/JDBC.java
@@ -2,15 +2,15 @@ import java.sql.*;
 
 public class JDBC {
 	public static void main(String[] args) throws Exception {
-		Class.forName("com.mysql.jbdc.Driver");
-		Connection con = DriverManager.getConnection("jbdc:mysql://localhost:3306/second31155","root","2368");
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/second31155","root","root");
 		
 		Statement st = con.createStatement();
 		ResultSet rs = st.executeQuery("Select * from Departments where dept_id=1");
 		
 		rs.next();
 		int dept_id=rs.getInt("Dept_id");
-		String dept_name=rs.getString("Dep_Name");
+		String dept_name=rs.getString("Dept_Name");
 		System.out.println(dept_id+" "+ dept_name);
 		st.close();
 		con.close();
