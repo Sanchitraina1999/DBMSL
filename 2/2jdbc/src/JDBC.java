0e25db902beb98d13d589aa1fa7424b05a34a4ac
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-09-04 10:47:44 +0530

    :apple:

--- 2/2jdbc/src/JDBC.java
+++ 2/2jdbc/src/JDBC.java
@@ -5,13 +5,22 @@ public class JDBC {
 		Class.forName("com.mysql.jdbc.Driver");
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/second31155","root","root");
 		
-		Statement st = con.createStatement();
-		ResultSet rs = st.executeQuery("Select * from Departments where dept_id=1");
 		
-		rs.next();
-		int dept_id=rs.getInt("Dept_id");
-		String dept_name=rs.getString("Dept_Name");
-		System.out.println(dept_id+" "+ dept_name);
+//		VIEW FOR -
+//		2. Display all professors details with city pune and mumbai and professor first name starting with
+//		a' or 'd'.
+
+		
+		Statement st = con.createStatement();
+		int rs = st.executeUpdate("CREATE VIEW prof_pune_mumbai_a_d AS\r\n" + 
+				"		SELECT * FROM Professors\r\n" + 
+				"		WHERE ((city='pune' or city='mumbai') and (prof_fname like 'a%' or prof_fname like 'd%'));");
+		
+//		rs.next();
+//		int dept_id=rs.getInt("Dept_id");
+//		String dept_name=rs.getString("Dept_Name");
+//		System.out.println(dept_id+" "+ dept_name);
+		
 		st.close();
 		con.close();
 	}
