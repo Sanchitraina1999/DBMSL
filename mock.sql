93c432df9baf4f3902d1b36d686e3057cb0ed2a6
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-10-13 15:20:43 +0530

    update: master

--- mock.sql
+++ mock.sql
@@ -5,7 +5,7 @@ USE MOCK;
 CREATE TABLE Emp(
     Emp_name VARCHAR(16) NOT NULL,
     City VARCHAR(16) NOT NULL,
-    EmpId INT NOT NULL AUTO_INCREMENT,
+    EmpId INT NOT NULL,
     PRIMARY KEY(Emp_name)
 );
 
@@ -24,44 +24,52 @@ CREATE TABLE Emp_Details(
     HIREDATE DATE NOT NULL,
     SAL INT NOT NULL,
     DEPTNO INT NOT NULL,
+    FOREIGN KEY (EMPNO) REFERENCES Emp(EmpId) ON DELETE CASCADE,
     FOREIGN KEY (ENAME) REFERENCES Emp(Emp_name) ON DELETE CASCADE,
     FOREIGN KEY (SAL) REFERENCES Emp_salary(Salary) ON DELETE CASCADE
 );
 
-
-DEPT - SMITH
-SALARY MORE THAN SALARY OF FORD USING EMP_DETAILS
-Display records of employee who are senior to JONES using Emp_Details relation. 
-Emp_View whose EmpId id 10. 
-
 INSERT INTO Emp
-    (Emp_name,City)
+    (Emp_name,City,EmpId)
 VALUES
-    ('Sanchit','Jammu'),
-    ('Sanchit','Jammu'),
-    ('Sanchit','Jammu'),
-    ('Sanchit','Jammu'),
-    ('Sanchit','Jammu'),
-    ('Sanchit','Jammu'),
-    ('Sanchit','Jammu'),
-    ('Sanchit','Jammu'),
-    ('Sanchit','Jammu'),
-    ('Sanchit','Jammu'),
-
-    (1, 1, 5),
-    (2, 2, 4),
-    (3, 3, 4),
-    (4, 1, 5),
-    (5, 2, 4),
-    (6, 3, 20),
-    (7, 2, 4),
-    (8, 3, 5),
-    (9, 1, 23),
-    (10, 2, 5);
-
-
+    ('Sanchit','Jammu',1),
+    ('Ayush','Pune',2),
+    ('Abhishek','Delhi',3),
+    ('JONES','Mumbai',4),
+    ('Ayushi','Nagpur',5),
+    ('FORD','Ambala',6),
+    ('SMITH','Badharwah',7),
+    ('Kabir','Poonch',8),
+    ('Kalpit','Rajouri',9),
+    ('Vansh','Srinagar',10);
  
+INSERT INTO Emp_salary
+    (EMPNO,Emp_name,Deptname,Salary)
+VALUES
+    (1,'Sanchit','IT',15000),
+    (2,'Ayush','ADMIN',10000),
+    (3,'Abhishek','SECURITY',12000),
+    (4,'JONES','SERVICE',15999),
+    (5,'Ayushi','IT',12000),
+    (6,'FORD','SERVICE',9000),
+    (7,'SMITH','ADMIN',25000),
+    (8,'Kabir','IT',23000),
+    (9,'Kalpit','SERVICE',24000),
+    (10,'Vansh','SECURITY',14000);
 
+INSERT INTO Emp_Details
+    (ENAME,JOB,HIREDATE,SAL,DEPTNO)
+VALUES
+    ('Sanchit','Jammu','1999-12-08',15000,1),
+    ('Ayush','Pune','1999-10-14',15000,2),
+    ('Abhishek','Delhi','2001-12-13',15000,3),
+    ('JONES','Mumbai','2002-12-12',15000,4),
+    ('Ayushi','Nagpur','2003-12-18',15000,1),
+    ('FORD','Ambala','2004-04-18',15000,4),
+    ('SMITH','Badharwah','1997-11-28',15000,2),
+    ('Kabir','Poonch','2006-01-30',15000,1),
+    ('Kalpit','Rajouri','1999-02-28',15000,4),
+    ('Vansh','Srinagar','1996-06-18',15000,3);
  
 
 1.List the employee details along with Deptname and salary ,
